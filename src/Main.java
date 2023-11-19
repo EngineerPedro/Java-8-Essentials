bb2767a16b25c01e238d49d657dd19f64bf6962a
Pedro Lebrpon Guzman III <plguzman1993@gmail.com>
2023-11-19 15:35:52 -0800

    Working with Character Values

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,23 @@
+
 // Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
 // then press Enter. You can now see whitespace characters in your code.
 public class Main {
     public static void main(String[] args) {
-        // Press Alt+Enter with your caret at the highlighted text to see how
-        // IntelliJ IDEA suggests fixing it.
-        System.out.println("Hello World");
-        System.out.println("Test");
+        char c1 = '1';
+        char c2 = '2';
+        char c3 = '3';
+        System.out.println("Char 1: " + c1);
+        System.out.println("Char 2: " + c2);
+        System.out.println("Char 3: " + c3);
+
+        char dollarSign = '$';
+        System.out.println("Currency: " + dollarSign);
+
+        char a1 = 'a';
+        char a2 = 'b';
+        char a3 = 'c';
+        System.out.print(Character.toUpperCase(a1));
+        System.out.print(Character.toUpperCase(a2));
+        System.out.println(Character.toUpperCase(a3));
     }
 }
