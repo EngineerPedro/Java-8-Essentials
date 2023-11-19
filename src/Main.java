9f220dbad839c50530a0fd8b74eb6ebd01d92b59
Pedro Lebrpon Guzman III <plguzman1993@gmail.com>
2023-11-19 15:55:18 -0800

    Lesson 5.4 Building a string from multiple values

--- src/Main.java
+++ src/Main.java
@@ -1,18 +1,30 @@
 import java.text.NumberFormat;
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        int intValue = 42;
-        String fromInt = Integer.toString(intValue);
-        System.out.println(fromInt);
+        String str1 = "Hello";
+        String str2 = "World";
+        String str3 = str1 + ", " + str2 + "!";
+        System.out.println(str3);
 
-        boolean boolValue = true;
-        String fromBool = Boolean.toString(boolValue);
-        System.out.println(fromBool);
+        StringBuilder sb = new StringBuilder("Hello")
+                .append(", ")
+                .append("World")
+                .append("!");
+        System.out.println(sb);
 
-        long longValue = 10_000_000;
-        NumberFormat formatter = NumberFormat.getNumberInstance();
-        String formatted = formatter.format(longValue);
-        System.out.println(formatted);
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter value: ");
+        String input = scanner.nextLine();
+        System.out.println(input);
+
+        sb.delete(0, sb.length());
+        for (int i = 0; i < 3; i++) {
+            input = scanner.nextLine();
+            sb.append(input + "\n");
+        }
+
+        System.out.println(sb);
     }
 }
