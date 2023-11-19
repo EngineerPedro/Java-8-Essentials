0ff0a18e0a2a2a18e25cc44f5dc75474ad385737
Pedro Lebrpon Guzman III <plguzman1993@gmail.com>
2023-11-19 15:53:39 -0800

    Lesson 5.3 Converting primitive value of strings

--- src/Main.java
+++ src/Main.java
@@ -1,26 +1,18 @@
-
+import java.text.NumberFormat;
 
 public class Main {
     public static void main(String[] args) {
-        String s1 = "This is a String!";
-        System.out.println(s1);
+        int intValue = 42;
+        String fromInt = Integer.toString(intValue);
+        System.out.println(fromInt);
 
-        String s2 = new String("This is also a String!");
-        System.out.println(s2);
+        boolean boolValue = true;
+        String fromBool = Boolean.toString(boolValue);
+        System.out.println(fromBool);
 
-        String s3 = "Shirt size: ";
-        String s4 = "M";
-        String s5 = s3 + s4 + ", Qty: " + 4;
-        System.out.println(s5);
-
-        char[] chars = {'H', 'e', 'l', 'l', 'o'};
-        String s6 = new String(chars);
-        System.out.println(s6);
-
-        char[]  chars2 = s6.toCharArray();
-        for (char c : chars2)
-        {
-            System.out.println(c);
-        }
+        long longValue = 10_000_000;
+        NumberFormat formatter = NumberFormat.getNumberInstance();
+        String formatted = formatter.format(longValue);
+        System.out.println(formatted);
     }
 }
