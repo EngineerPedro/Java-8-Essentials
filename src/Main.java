a81c2474fdd530f4c19cf783eb9f44429c5909fb
Pedro Lebrpon Guzman III <plguzman1993@gmail.com>
2023-11-19 16:48:53 -0800

    Lesson 6.1 Understanding syntax errors vs. runtime errors

--- src/Main.java
+++ src/Main.java
@@ -10,18 +10,14 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
+        String s = null;
 
-        System.out.print("Enter a numeric value: ");
-        String input1 = sc.nextLine();
-        double d1 = Double.parseDouble(input1);
+        System.out.println(s);
 
-        System.out.print("Enter a numeric value: ");
-        String input2 = sc.nextLine();
-        double d2 = Double.parseDouble(input2);
+        String welcome = "Welcome!";
+        char[] chars = welcome.toCharArray();
+        char lastChar = chars[chars.length - 1];
+        System.out.println(lastChar);
 
-        double result = d1 + d2;
-
-        System.out.println("The answer is " + result);
     }
 }
