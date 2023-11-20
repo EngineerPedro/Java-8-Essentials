da1989bb707fd9bf09af72f8ca44aefd58b30798
Pedro Lebrpon Guzman III <plguzman1993@gmail.com>
2023-11-19 17:47:39 -0800

    Lesson 8.2 using two dimensional arrays

--- src/Main.java
+++ src/Main.java
@@ -4,34 +4,23 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
 
-        System.out.println("Array of primitives");
-        int[] ints = {9, 6, 3};
-        Arrays.sort(ints);
-        for (int i = 0; i < ints.length; i++) {
-            System.out.println(ints[i]);
+        String[][] states = new String[3][2];
+        states[0][0] = "California";
+        states[0][1] = "Sacramento";
+        states[1][0] = "Oregon";
+        states[1][1] = "Salem";
+        states[2][0] = "Washington";
+        states[2][1] = "Olympia";
+
+        for (int i = 0; i < states.length; i++) {
+            StringBuilder sb = new StringBuilder();
+            sb.append("The capitol of ")
+                    .append(states[i][0])
+                    .append(" is ")
+                    .append(states[i][1])
+                    .append(".");
+            System.out.println(sb);
         }
 
-        System.out.println("Array of strings");
-        String[] strings = {"Red", "Green", "Blue"};
-        Arrays.sort(strings);
-        for (String color : strings) {
-            System.out.println(color);
-        }
-
-        System.out.println("Setting an initial size");
-        int[] sized = new int[10];
-        for (int i = 0; i < sized.length; i++) {
-            sized[i] = i * 100;
-        }
-        for (int value : sized) {
-            System.out.println(value);
-        }
-
-        System.out.println("Copying an array");
-        int[] copied = new int[5];
-        System.arraycopy(sized, 5, copied, 0, 5);
-        for (int value : copied) {
-            System.out.println(value);
-        }
     }
 }
