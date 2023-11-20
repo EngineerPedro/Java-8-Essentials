3be6295c1368a7f33b71105e66c5b5782ea6948c
Pedro Lebrpon Guzman III <plguzman1993@gmail.com>
2023-11-19 17:03:25 -0800

    Lesson 6.3  handling exceptions with try catch

--- src/Main.java
+++ src/Main.java
@@ -1,20 +1,14 @@
-import java.text.DateFormat;
-import java.text.NumberFormat;
-import java.time.LocalDate;
-import java.time.LocalDateTime;
-import java.time.format.DateTimeFormatter;
-import java.util.Date;
-import java.util.GregorianCalendar;
-import java.util.Locale;
-import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
         String welcome = "Welcome!";
         char[] chars = welcome.toCharArray();
 
-        for (int i = 0; i < chars.length; i++) {
-            System.out.println(chars[i]);
+        try {
+            char lastChar = chars[chars.length - 1];
+            System.out.println(lastChar);
+        } catch (ArrayIndexOutOfBoundsException e) {
+            e.printStackTrace();
         }
     }
 }
