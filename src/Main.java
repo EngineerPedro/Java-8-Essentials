3b85afc8d79f029d968e30e6054406afd4325f38
Pedro Lebrpon Guzman III <plguzman1993@gmail.com>
2023-11-19 17:33:56 -0800

    Lesson 7.5 OverLoading methods with different signatures

--- src/Main.java
+++ src/Main.java
@@ -1,28 +1,51 @@
 import java.util.Scanner;
 
 public class Main {
-        static String[] months = {"January", "February", "March",
-                "April", "May", "June",
-                "July", "August", "September",
-                "October", "November", "December"};
+    public static void main(String[] args) {
+        String s1 = getInput("Enter value 1: ");
+        String s2 = getInput("Enter value 2: ");
+        String s3 = getInput("Enter value 3: ");
 
-        public static void main(String[] args) {
-            loopIt("Months of the year");
-            loopIt("Second run");
-            loopIt("Third run");
+        double result = addValues(s1, s2);
+        System.out.println("The answer is " + result);
+
+        double result2 = addValues(s1, s2, s3);
+        System.out.println("The answer for 3 values is " + result2);
+
+        double result3 = addValues(s1, s2, s3, s1, s2, s3);
+        System.out.println("The answer for many values is " + result3);
+
+    }
+
+    static String getInput(String prompt) {
+        System.out.print(prompt);
+        Scanner sc = new Scanner(System.in);
+
+        return sc.nextLine();
+    }
+
+    static double addValues(String s1, String s2) {
+        double d1 = Double.parseDouble(s1);
+        double d2 = Double.parseDouble(s2);
+        double result = d1 + d2;
+        return result;
+    }
+
+    static double addValues(String s1, String s2, String s3) {
+        double d1 = Double.parseDouble(s1);
+        double d2 = Double.parseDouble(s2);
+        double d3 = Double.parseDouble(s3);
+        double result = d1 + d2 + d3;
+        return result;
+    }
+
+    static double addValues(String ... values) {
+        double result = 0;
+        for (String value : values) {
+            double d = Double.parseDouble(value);
+            result += d;
         }
-
-        static void loopIt(String label) {
-
-            System.out.println(label);
-            for (int i = 0; i < label.length(); i++) {
-                System.out.print("*");
-            }
-            System.out.println("");
-
-            for (int i = 0; i < months.length; i++) {
-                System.out.println(months[i]);
-            }
-        }
+        return result;
+    }
 
 }
