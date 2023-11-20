24504c165f1be295f6afba94499e419238d39641
Pedro Lebrpon Guzman III <plguzman1993@gmail.com>
2023-11-19 17:05:49 -0800

    Lesson 6.4 Creating multiple catch blocks

--- src/Main.java
+++ src/Main.java
@@ -7,8 +7,14 @@ public class Main {
         try {
             char lastChar = chars[chars.length - 1];
             System.out.println(lastChar);
+
+            String sub = welcome.substring(10);
+
         } catch (ArrayIndexOutOfBoundsException e) {
-            e.printStackTrace();
+//            e.printStackTrace();
+            System.out.println("Array index problem!");
+        } catch (StringIndexOutOfBoundsException e) {
+            System.out.println("String index problem!");
         }
     }
 }
