6b25554859d481132bf37d2d633cd0d561e8a325
Pedro Lebrpon Guzman III <plguzman1993@gmail.com>
2023-11-27 08:25:06 -0800

    Lesson 9.9 Declaring and using Enum types

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,5 @@
 import model.Olive;
+import model.OliveName;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -8,9 +9,9 @@ public class Main {
     public static void main(String[] args) {
 
         List<Olive> olives = new ArrayList<>();
-        olives.add(new Olive(Olive.KALAMATA, 0x2E0854, 3));
-        olives.add(new Olive(Olive.KALAMATA, 0x2E0854, 7));
-        olives.add(new Olive(Olive.KALAMATA, 0x2E0854, 12));
+        olives.add(new Olive(OliveName.KALAMATA, 0x2E0854, 3));
+        olives.add(new Olive(OliveName.KALAMATA, 0x2E0854, 7));
+        olives.add(new Olive(OliveName.KALAMATA, 0x2E0854, 12));
 
         OlivePress press = new OlivePress();
         int totalOil = press.getOil(olives);
